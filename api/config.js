eb29e33697590c489da38eacaf7eeaa54edc4474
martinhogsgaard <mh@weproduce.dk>
2026-03-03 14:33:38 +0100

    Delete api/config.js

--- api/config.js
+++
@@ -1,9 +0,0 @@
-// api/config.js
-// Sender APP_KEY sikkert til frontend så den kan autentificere mod proxy.
-// APP_KEY er aldrig synlig i kildekoden - kun i Vercel environment variables.
-
-export default function handler(req, res) {
-  res.status(200).json({
-    appKey: process.env.APP_KEY || ""
-  });
-}
